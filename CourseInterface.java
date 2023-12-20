4e2ab7554303168f1d1cee9d810a920f3a517432
A. Burris <87954469+Eighlian@users.noreply.github.com>
2023-12-20 16:49:13 -0600

    Create CourseInterface.java

---
+++ CourseInterface.java
@@ -0,0 +1,10 @@
+import java.sql.SQLException;
+
+public interface CourseInterface extends GenericInterface<Course> {
+	
+	Course assignRoom(String type, String number, String rType, String rNum) throws SQLException;
+
+	Course registerStudents(String type, String level, int students) throws SQLException;
+
+	int getStudents(String type, String number) throws SQLException;
+}
